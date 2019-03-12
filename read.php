90a965759f9e0d7a84af032a5f094f7129f36f71
Galaxy740 <45127098+Galaxy740@users.noreply.github.com>
2019-03-12 22:45:57 +0200

    as

--- read.php
+++ read.php
@@ -1,8 +1,9 @@
 <?php
 
 session_start();
-include "includes/header.php";
 $title = 'Read';
+include "includes/header.php";
+
 
 $q_read = "SELECT `date_departure` dd,destination_point dp, `flights_id`, (seats-prurchases_seats) AS Available FROM `flight` f JOIN plane p ON p.planes_id=f.planes_id JOIN destination";
 $result = mysqli_query($conn, $q_read);
@@ -63,3 +64,33 @@ $result = mysqli_query($conn, $q_read);
  
 include 'includes/footer.php';
 ?>
+<div class="table100 ver3 m-b-110">
+    <table data-vertable="ver3">
+        <thead>
+        <tr class="row100 head">
+            <th class="column100 column1" data-column="column1"></th>
+            <th class="column100 column2" data-column="column2">Sunday</th>
+            <th class="column100 column3" data-column="column3">Monday</th>
+            <th class="column100 column4" data-column="column4">Tuesday</th>
+            <th class="column100 column5" data-column="column5">Wednesday</th>
+            <th class="column100 column6" data-column="column6">Thursday</th>
+            <th class="column100 column7" data-column="column7">Friday</th>
+            <th class="column100 column8" data-column="column8">Saturday</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="row100">
+            <td class="column100 column1" data-column="column1">Lawrence Scott</td>
+            <td class="column100 column2" data-column="column2">8:00 AM</td>
+            <td class="column100 column3" data-column="column3">--</td>
+            <td class="column100 column4" data-column="column4">--</td>
+            <td class="column100 column5" data-column="column5">8:00 AM</td>
+            <td class="column100 column6" data-column="column6">--</td>
+            <td class="column100 column7" data-column="column7">5:00 PM</td>
+            <td class="column100 column8" data-column="column8">8:00 AM</td>
+        </tr>
+
+    
+        </tbody>
+    </table>
+</div>
